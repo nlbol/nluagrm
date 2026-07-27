f1cb6c2bfffd00e9475f744c7bfc52dabbf99507
FershoUno <fersh0un0@gmail.com>
2026-07-26 23:17:45 -0400

    updated

---
+++ src/config/footer.ts
@@ -0,0 +1,12 @@
+export const footerPlatforms = [
+	{ name: "Pagina Principal", url: "https://nluagrm.org" },
+	{ name: "Biblioteca", url: "https://biblioteca.nluagrm.org" },
+	{ name: "Meet", url: "https://meet.nluagrm.org" },
+	{ name: "Wiki", url: "https://wiki.nluagrm.org" },
+];
+
+export const footerSocials = [
+	{ name: "Facebook · Grupo", url: "https://www.facebook.com/groups/nucleolinux.uagrm" },
+	{ name: "YouTube", url: "https://www.youtube.com/@nucleolinuxuagrm9768" },
+	{ name: "Telegram", url: "https://t.me/nucleolinux_uagrm" },
+];
